6512aeb4169024001c570b66e500a98baedd7a95
ForLearningAtJob4J <youcanwriteme@yandex.ru>
2021-07-18 18:26:27 +0700

    0. Что такое интеграционное тестирование. [#6875]

---
+++ src/test/java/ru/job4j/integration/OrdersStoreTest.java
@@ -0,0 +1,53 @@
+package ru.job4j.integration;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class OrdersStoreTest {
+    private final BasicDataSource pool = new BasicDataSource();
+
+    @Before
+    public void setUp() throws SQLException {
+        pool.setDriverClassName("org.hsqldb.jdbcDriver");
+        pool.setUrl("jdbc:hsqldb:mem:tests;sql.syntax_pgs=true");
+        pool.setUsername("sa");
+        pool.setPassword("");
+        pool.setMaxTotal(2);
+        executeDdl("./db/update_001.sql");
+    }
+
+    private void executeDdl(String script) throws SQLException {
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(new FileInputStream(script)))
+        ) {
+            br.lines().forEach(line -> builder.append(line).append(System.lineSeparator()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        pool.getConnection().prepareStatement(builder.toString()).executeUpdate();
+    }
+
+    @Test
+    public void whenSaveOrderAndFindAllOneRowWithDescription() {
+        OrdersStore store = new OrdersStore(pool);
+
+        store.save(Order.of("name1", "description1"));
+
+        List<Order> all = (List<Order>) store.findAll();
+
+        assertEquals(1, all.size());
+        assertEquals("description1", all.get(0).getDescription());
+        assertEquals(1, all.get(0).getId());
+    }
+}
